07bd34ce84f561f1ea98b1b8d2fdf69d51d2719e
andrehideki <andre.hkiw.dev@gmail.com>
2025-02-25 09:16:45 -0300

    add: demo-docker

---
+++ demo-docker/src/main/java/br/com/lab/demo_docker/AppController.java
@@ -0,0 +1,15 @@
+package br.com.lab.demo_docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class AppController {
+
+	@GetMapping
+	public String get() {
+		return "hello";
+	}
+}
